361ac4e10384e23e35abf111f52b9035e9f2f62a
Sara Harvey <saraharveynyc@gmail.com>
2019-07-20 18:48:09 +0000

    Automatically backed up by Learn

---
+++ app/models/show.rb
@@ -0,0 +1,19 @@
+class Show < ActiveRecord::Base
+
+  def self.highest_rating
+    show = Show.maximum(:rating)
+  end
+
+  def self.most_popular_show
+    show = Show.maximum(:rating)
+    most_popular_show = Show.find_by(rating: show)
+  end
+
+  def self.most_popular_show
+    show = Show.maximum(:rating)
+    most_popular_show = Show.find_by(rating: show)
+  end
+
+  
+
+end
